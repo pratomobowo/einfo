b3c044189bd84476670dc51de40d3d743318247d
pratomobowo <pratomobowo@gmail.com>
2025-05-05 14:38:19 +0700

    Tambah informasi admin pembuat kegiatan pada modal detail activity

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -137,7 +137,7 @@
                                     </p>
                                 </div>
                                 <div class="mt-2 flex items-center text-sm sm:mt-0">
-                                    <button @click="activeActivity = {{ json_encode($activity->append('formatted_date')) }}; showModal = true" class="text-white bg-blue-600 hover:bg-blue-700 rounded-md px-3 py-1 text-sm transition-colors duration-150 flex items-center">
+                                    <button @click="activeActivity = {{ json_encode($activity->append(['formatted_date', 'creator'])) }}; showModal = true" class="text-white bg-blue-600 hover:bg-blue-700 rounded-md px-3 py-1 text-sm transition-colors duration-150 flex items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
                                             <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
                                             <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
@@ -207,7 +207,7 @@
                                     </p>
                                 </div>
                                 <div class="mt-2 flex items-center text-sm sm:mt-0">
-                                    <button @click="activeActivity = {{ json_encode($activity->append('formatted_date')) }}; showModal = true" class="text-white bg-blue-600 hover:bg-blue-700 rounded-md px-3 py-1 text-sm transition-colors duration-150 flex items-center">
+                                    <button @click="activeActivity = {{ json_encode($activity->append(['formatted_date', 'creator'])) }}; showModal = true" class="text-white bg-blue-600 hover:bg-blue-700 rounded-md px-3 py-1 text-sm transition-colors duration-150 flex items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
                                             <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
                                             <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
@@ -277,7 +277,7 @@
                                     </p>
                                 </div>
                                 <div class="mt-2 flex items-center text-sm sm:mt-0">
-                                    <button @click="activeActivity = {{ json_encode($activity->append('formatted_date')) }}; showModal = true" class="text-white bg-blue-600 hover:bg-blue-700 rounded-md px-3 py-1 text-sm transition-colors duration-150 flex items-center">
+                                    <button @click="activeActivity = {{ json_encode($activity->append(['formatted_date', 'creator'])) }}; showModal = true" class="text-white bg-blue-600 hover:bg-blue-700 rounded-md px-3 py-1 text-sm transition-colors duration-150 flex items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
                                             <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
                                             <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
@@ -392,6 +392,21 @@
                             <div class="ml-3">
                                 <h4 class="text-sm font-medium text-gray-900">Lokasi</h4>
                                 <p class="text-sm text-gray-600" x-text="activeActivity ? activeActivity.location : ''"></p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- Creator Information -->
+                    <div class="border-b border-gray-200 py-4">
+                        <div class="flex items-start">
+                            <div class="flex-shrink-0 bg-blue-100 rounded-full p-2">
+                                <svg class="h-5 w-5 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <h4 class="text-sm font-medium text-gray-900">Dibuat Oleh</h4>
+                                <p class="text-sm text-gray-600" x-text="activeActivity && activeActivity.creator ? activeActivity.creator.name : 'Admin'"></p>
                             </div>
                         </div>
                     </div>
